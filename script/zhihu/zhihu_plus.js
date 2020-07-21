5f38be118687ec201f7bb407efa4d26ac351c68d
blackmatrix7 <27717518+blackmatrix7@users.noreply.github.com>
2020-07-22 01:03:49 +0800

    引入MagicJS

--- script/zhihu/zhihu_plus.js
+++ script/zhihu/zhihu_plus.js
@@ -8,34 +8,33 @@ hostname = api.zhihu.com
 
 [Script]
 知乎_用户信息去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https://api.zhihu.com/people/,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
-知乎_信息流去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https://api.zhihu.com/moments/recommend,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
-知乎_推荐去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https://api.zhihu.com/topstory/recommend,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
+知乎_信息流去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https://api.zhihu.com/(moments|topstory)/recommend,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
 知乎_回答黑名单增强 = type=http-response,requires-body=1,max-size=0,pattern=^https://api.zhihu.com/v4/questions,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
-知乎_官方消息去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https:\/\/api.zhihu.com\/notifications\/v3\/message\?,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
-知乎_消息列表去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https:\/\/api.zhihu.com\/notifications\/v3\/timeline\/entry\/system_message,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
+知乎_官方消息去广告 = type=http-response,requires-body=1,max-size=0,pattern=^https:\/\/api.zhihu.com\/notifications\/v3\/(message\?|timeline\/entry\/system_message),script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zhihu/zhihu_plus.js
 */
 
-let body = $response.body;
+let magicJS = MagicJS('知乎增强');
+let body = magicJS.response.body;
 try{
   if (body.length > 0){
     body=JSON.parse(body);
   }
 }
 catch (err){
-  console.log('[知乎去广告] 解析body出现异常：' + $response.body);
+  magicJS.log('[知乎去广告] 解析body出现异常：' + $response.body);
   body = {};
 }
 
-let topstory_recommend_regex = /^https:\/\/api\.zhihu\.com\/topstory\/recommend\?/;
-let moments_recommend_regex = /^https:\/\/api.zhihu.com\/moments\/recommend/;
-let mcn_userinfo = /^https:\/\/api.zhihu.com\/people\//;
-let question_regex = /^https:\/\/api.zhihu.com\/v4\/questions/;
-let sysmsg_timeline_regex = /^https:\/\/api.zhihu.com\/notifications\/v3\/timeline\/entry\/system_message/;
-let sysmsg_notifications_regex = /^https:\/\/api.zhihu.com\/notifications\/v3\/message\?/;
-let answer_blacklist = ['盐选推荐', '盐选科普', '会员推荐', '故事档案局', '小蒜苗', '魏甚麽'];
-let sysmsg_blacklist = ['知乎小伙伴', '知乎视频', '知乎亲子', '知乎团队', '知乎好物推荐', '知乎盐选会员', '知乎礼券', '创作者小助手', '知乎校园'];
+const topstory_recommend_regex = /^https:\/\/api\.zhihu\.com\/topstory\/recommend\?/;
+const moments_recommend_regex = /^https:\/\/api.zhihu.com\/moments\/recommend/;
+const mcn_userinfo = /^https:\/\/api.zhihu.com\/people\//;
+const question_regex = /^https:\/\/api.zhihu.com\/v4\/questions/;
+const sysmsg_timeline_regex = /^https:\/\/api.zhihu.com\/notifications\/v3\/timeline\/entry\/system_message/;
+const sysmsg_notifications_regex = /^https:\/\/api.zhihu.com\/notifications\/v3\/message\?/;
+const answer_blacklist = ['盐选推荐', '盐选科普', '会员推荐', '故事档案局', '小蒜苗', '魏甚麽'];
+const sysmsg_blacklist = ['知乎小伙伴', '知乎视频', '知乎亲子', '知乎团队', '知乎好物推荐', '知乎盐选会员', '知乎礼券', '创作者小助手', '知乎校园'];
 
-if (topstory_recommend_regex.test($request.url)){
+if (topstory_recommend_regex.test(magicJS.request.url)){
   body['data'].forEach((element, index)=> {
     if(element['card_type']=='slot_event_card'||element.hasOwnProperty('ad')){      
       body['data'].splice(index,1);
@@ -43,7 +42,7 @@ if (topstory_recommend_regex.test($request.url)){
   });
 }
 // 推荐功能去广告及黑名单增强
-else if (moments_recommend_regex.test($request.url)){
+else if (moments_recommend_regex.test(magicJS.request.url)){
   let data = body['data'].filter((element) =>{
     try{
       if(element.hasOwnProperty('ad') == false || (
@@ -53,17 +52,17 @@ else if (moments_recommend_regex.test($request.url)){
       }
     }
     catch (err){
-      console.log('[知乎去广告] 推荐去广告出现异常：' + err);
+      magicJS.log('[知乎去广告] 推荐去广告出现异常：' + err);
       return true;
     }
   });
   body['data'] = data;
 }
-else if (mcn_userinfo.test($request.url)){
+else if (mcn_userinfo.test(magicJS.request.url)){
   delete body['mcn_user_info']
 }
 // 知乎黑名单增强，在回答列表里不会出现黑名单的答主
-else if (question_regex.test($request.url)){
+else if (question_regex.test(magicJS.request.url)){
   delete body['ad_info'];
   let data = body['data'].filter((element) =>{
     if (answer_blacklist.indexOf(element['author']['name']) < 0){
@@ -73,7 +72,7 @@ else if (question_regex.test($request.url)){
   body['data'] = data;
 }
 // 拦截官方账号推广消息
-else if (sysmsg_timeline_regex.test($request.url) && body.hasOwnProperty('data')){
+else if (sysmsg_timeline_regex.test(magicJS.request.url) && body.hasOwnProperty('data')){
   let data = body['data'].filter((element) =>{
     if (sysmsg_blacklist.indexOf(element['content']['title']) < 0){
       return true;
@@ -81,7 +80,7 @@ else if (sysmsg_timeline_regex.test($request.url) && body.hasOwnProperty('data')
   })
   body['data'] = data;
 } 
-else if (sysmsg_notifications_regex.test($request.url)){
+else if (sysmsg_notifications_regex.test(magicJS.request.url)){
   body['data'].forEach((element, index)=> {
     if(element['detail_title']=='官方帐号消息'){
       let unread_count = body['data'][index]['unread_count'];
@@ -97,4 +96,306 @@ else if (sysmsg_notifications_regex.test($request.url)){
   })
 }
 body=JSON.stringify(body);
-$done({body});
+magicJS.done({body});
+
+function MagicJS(scriptName='MagicJS', debug=false){
+  return new class{
+
+    constructor(){
+      this.scriptName = scriptName;
+      this.debug = debug;
+      this.node = {'request': undefined, 'fs': undefined, 'data': {}};
+      if (this.isNode){
+        this.node.request = require('request');
+        this.node.data = require('./magic.json');
+        this.node.fs = require('fs');
+      }
+    }
+    
+    get version() { return '202007220021' };
+    get isSurge() { return typeof $httpClient !== 'undefined' && !this.isLoon };
+    get isQuanX() { return typeof $task !== 'undefined' };
+    get isLoon() { return typeof $loon !== 'undefined' };
+    get isJSBox() { return typeof $drive !== 'undefined'};
+    get isNode() { return typeof module !== 'undefined' && !this.isJSBox };
+    get isRequest() { return (typeof $request !== 'undefined') && (typeof $response === 'undefined')}
+    get isResponse() { return typeof $response !== 'undefined' }
+    get request() { return (typeof $request !== 'undefined') ? $request : undefined }
+
+
+    get response() { 
+      if (typeof $response !== 'undefined'){
+        if ($response.hasOwnProperty('status')) $response['statusCode'] = $response['status']
+        if ($response.hasOwnProperty('statusCode')) $response['status'] = $response['statusCode']
+        return $response;
+      }
+      else{
+        return undefined;
+      }
+    }
+
+    read(key, session='default'){
+      let data = '';
+      if (this.isSurge || this.isLoon) {
+        data = $persistentStore.read(key);
+      }
+      else if (this.isQuanX) {
+        data = $prefs.valueForKey(key);
+      }
+      else if (this.isNode){
+        data = this.node.data[key];
+      }
+      else if (this.isJSBox){
+        data = $file.read('drive://magic.json').string;
+        data = JSON.parse(data)[key];
+      }
+      try {
+        if (typeof data === 'string'){
+          data = JSON.parse(data);
+        }
+        data = data != null && data != undefined ? data: {};
+      } 
+      catch (err){ 
+        this.log(`Parse Data Error: ${err}`);
+        data = {};
+        this.del(key);
+      }
+      let val = data[session];
+      try { if (typeof val == 'string') val = JSON.parse(val) } catch {}
+      if (this.debug) this.log(`read data [${key}][${session}](${typeof val})\n${JSON.stringify(val)}`);
+      return val;
+    };
+
+    write(key, val, session='default'){
+      let data = '';
+      if (this.isSurge || this.isLoon) {
+        data = $persistentStore.read(key);
+      }
+      else if (this.isQuanX) {
+        data = $prefs.valueForKey(key);
+      }
+      else if (this.isNode){
+        data = this.node.data;
+      }
+      else if (this.isJSBox){
+        data = JSON.parse($file.read('drive://magic.json').string);
+      }
+      try {
+        if (typeof data === 'string'){
+          data = JSON.parse(data);
+        }
+        data = data != null && data != undefined ? data: {};
+      } 
+      catch(err) { 
+        this.log(`Parse Data Error: ${err}`);
+        data = {};
+        this.del(key);
+      }
+      if (this.isNode || this.isJSBox){
+        data[key][session] = val;
+      }
+      else{
+        data[session] = val;
+      }
+      data = JSON.stringify(data);
+      if (this.isSurge || this.isLoon) {
+        $persistentStore.write(data, key);
+      }
+      else if (this.isQuanX) {
+        $prefs.setValueForKey(data, key);
+      }
+      else if (this.isNode){
+        this.node.fs.writeFileSync('./magic.json', data, (err) =>{
+          this.log(err);
+        })
+      }
+      else if (this.isJSBox){
+        $file.write({data: $data({string: data}), path: 'drive://magic.json'});
+      }
+      if (this.debug) this.log(`write data [${key}][${session}](${typeof val})\n${JSON.stringify(val)}`);
+    };
+
+    del(key){
+      if (this.isSurge || this.isLoon) {
+        $persistentStore.write({}, key);
+      }
+      else if (this.isQuanX) {
+        $prefs.setValueForKey({}, key);
+      }
+      else if (this.isNode || this.isJSBox){
+        this.write(key, '');
+      }
+    }
+
+    notify(title, subTitle = '', body = ''){
+      if (this.isSurge || this.isLoon) {
+        $notification.post(title, subTitle, body);
+      }
+      else if (this.isQuanX) {
+         $notify(title, subTitle, body);
+      }
+      else if (this.isNode) {
+        this.log(`${title} ${subTitle}\n${body}`);
+      }
+      else if (this.isJSBox){
+        $push.schedule({
+          title: title,
+          body: subTitle ? `${subTitle}\n${body}` : body
+        });
+      }
+    }
+    
+    log(msg){
+      console.log(`[${this.scriptName}]\n${msg}\n`)
+    }
+
+    get(options, callback){
+      if (this.debug) this.log(`http get: ${JSON.stringify(options)}`);
+      if (this.isSurge || this.isLoon) {
+        $httpClient.get(options, callback);
+      }
+      else if (this.isQuanX) {
+        if (typeof options === 'string') options = { url: options }
+        options['method'] = 'GET'
+        $task.fetch(options).then(
+          resp => {
+            resp['status'] = resp.statusCode
+            callback(null, resp, resp.body)
+          },
+          reason => callback(reason.error, null, null),
+        )
+      }
+      else if(this.isNode){
+        return this.node.request.get(options, callback);
+      }
+      else if(this.isJSBox){
+        options = typeof options === 'string'? {'url': options} : options;
+        options['header'] = options['headers'];
+        delete options['headers']
+        options['handler'] = (resp)=>{
+          let err = resp.error? JSON.stringify(resp.error) : undefined;
+          let data = typeof resp.data === 'object' ? JSON.stringify(resp.data) : resp.data;
+          callback(err, resp.response, data);
+        }
+        $http.get(options);
+      }
+    }
+
+    post(options, callback){
+      if (this.debug) this.log(`http post: ${JSON.stringify(options)}`);
+      if (this.isSurge || this.isLoon) {
+        $httpClient.post(options, callback);
+      }
+      else if (this.isQuanX) {
+        if (typeof options === 'string') options = { url: options }
+        if (options.hasOwnProperty('body') && typeof options['body'] !== 'string') options['body'] = JSON.stringify(options['body']);
+        options['method'] = 'POST'
+        $task.fetch(options).then(
+          resp => {
+            resp['status'] = resp.statusCode
+            callback(null, resp, resp.body)
+          },
+          reason => {callback(reason.error, null, null)}
+        )
+      }
+      else if(this.isNode){
+        if (typeof options.body === 'object') options.body = JSON.stringify(options.body);
+        return this.node.request.post(options, callback);
+      }
+      else if(this.isJSBox){
+        options = typeof options === 'string'? {'url': options} : options;
+        options['header'] = options['headers'];
+        delete options['headers']
+        options['handler'] = (resp)=>{
+          let err = resp.error? JSON.stringify(resp.error) : undefined;
+          let data = typeof resp.data === 'object' ? JSON.stringify(resp.data) : resp.data;
+          callback(err, resp.response, data);
+        }
+        $http.post(options);
+      }
+    }
+
+    done(value = {}){
+      if (typeof $done !== 'undefined'){
+        $done(value);
+      }
+    }
+
+    isToday(day){
+      if (day == null){
+          return false;
+      }
+      else{
+        let today = new Date();
+        if (typeof day == 'string'){
+            day = new Date(day);
+        }
+        if (today.getFullYear() == day.getFullYear() && today.getMonth() == day.getMonth() && today.getDay() == day.getDay()){
+            return true;
+        }
+        else{
+            return false;
+        }
+      }
+    }
+
+    /**
+     * 对await执行中出现的异常进行捕获并返回，避免写过多的try catch语句
+     * @param {*} promise Promise 对象
+     * @param {*} defaultValue 出现异常时返回的默认值
+     * @returns 返回两个值，第一个值为异常，第二个值为执行结果
+     */
+    attempt(promise, defaultValue=null){ return promise.then((args)=>{return [null, args]}).catch(ex=>{this.log('raise exception:' + ex); return [ex, defaultValue]})};
+
+    /**
+     * 重试方法
+     *
+     * @param {*} fn 需要重试的函数
+     * @param {number} [retries=5] 重试次数
+     * @param {number} [interval=0] 每次重试间隔
+     * @param {function} [callback=null] 函数没有异常时的回调，会将函数执行结果result传入callback，根据result的值进行判断，如果需要再次重试，在callback中throw一个异常，适用于函数本身没有异常但仍需重试的情况。
+     * @returns 返回一个Promise对象
+     */
+    retry(fn, retries=5, interval=0, callback=null) {
+      return (...args)=>{
+        return new Promise((resolve, reject) =>{
+          function _retry(...args){
+            Promise.resolve().then(()=>fn.apply(this,args)).then(
+              result => {
+                if (typeof callback === 'function'){
+                  Promise.resolve().then(()=>callback(result)).then(()=>{resolve(result)}).catch(ex=>{
+                    if (retries >= 1 && interval > 0){
+                      setTimeout(() => _retry.apply(this, args), interval);
+                    }
+                    else if (retries >= 1) {
+                      _retry.apply(this, args);
+                    }
+                    else{
+                      reject(ex);
+                    }
+                    retries --;
+                  });
+                }
+                else{
+                  resolve(result);
+                }
+              }
+              ).catch(ex=>{
+              if (retries >= 1 && interval > 0){
+                setTimeout(() => _retry.apply(this, args), interval);
+              }
+              else if (retries >= 1) {
+                _retry.apply(this, args);
+              }
+              else{
+                reject(ex);
+              }
+              retries --;
+            })
+          }
+          _retry.apply(this, args);
+        });
+      };
+    }
+  }(scriptName);
+}
