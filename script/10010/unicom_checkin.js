467ac963eace1c6c7e38d59f6483f68e824a4992
blackmatrix7 <27717518+blackmatrix7@users.noreply.github.com>
2020-08-16 10:33:38 +0800

    删除Surge中多余的配置

--- script/10010/unicom_checkin.js
+++ script/10010/unicom_checkin.js
@@ -3,8 +3,8 @@
 m.client.10010.com
 
 [Script]
-联通_获取cookie = type=http-request,pattern=^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,script-update-interval=0
-联通_签到与抽奖 = script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,script-update-interval=0,type=cron,cronexp=10 0 * * *
+联通_获取cookie = type=http-request,pattern=^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,
+联通_签到与抽奖 = script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/10010/unicom_checkin.js,,type=cron,cronexp=10 0 * * *
 */
 const getLotteryCookieRegex = /^https?:\/\/m\.client\.10010\.com\/dailylottery\/static\/(integral|doubleball)\/firstpage/;
 const unicomCookieKey = 'unicom_user_cookie';
